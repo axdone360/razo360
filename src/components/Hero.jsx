6c70c4687af60ccb43b173ef6547deee26071a7d
axdone360 <101874973+axdone360@users.noreply.github.com>
2026-02-10 00:03:36 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ export default function Hero() {
     <div className="bg-primary text-white py-12 md:py-16 lg:py-24 min-h-[700px] max-sm:min-h-[650px] relative flex flex-col items-center justify-center overflow-hidden">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center z-10">
         <p className="text-base sm:text-lg md:text-xl mb-2 text-yellow-500 font-bold">
-          B2B and B2C Scaling Specialists
+          whatsapp marketing and leads extraction heaven
         </p>
         <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-4 md:mb-6">
           Fix the Problems that stops your{" "}
